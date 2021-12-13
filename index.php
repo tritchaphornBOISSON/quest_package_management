67c9384db39bdad75e762bc7f6c678a5a7163fb1
Tritchaphorn BOISSON <tritchaphorn@gmail.com>
2021-12-13 11:02:53 +0100

    first submit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once 'vendor/autoload.php';
+use CowSay\Cow;
+
+$bessie = new Cow('Hello, Farm!');
+
+// store the output in a variable
+$output = $bessie->say();
+echo $output;
+
+// or just echo the object for direct output
+echo $bessie;
+
+$bessie = new Cow('Hello, Farm!');
+$bessie->setEyes('oO')
+    ->setTongue('U')
+    ->setPoop('@@@')
+    ->setUdder('W');
+echo $bessie;
---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => NULL,
+        'name' => 'tritchaphornboisson/package_management_quest',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'gipetto/cowsay' => array(
+            'pretty_version' => '1.1.0',
+            'version' => '1.1.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../gipetto/cowsay',
+            'aliases' => array(),
+            'reference' => '85218ee4c0d18cc8753e08af54a8740aac12860a',
+            'dev_requirement' => false,
+        ),
+        'tritchaphornboisson/package_management_quest' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => NULL,
+            'dev_requirement' => false,
+        ),
+    ),
+);
